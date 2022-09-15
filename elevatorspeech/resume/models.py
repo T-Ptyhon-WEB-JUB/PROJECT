1a2f6872f40130f85556e06f3699966ca4cc7ce6
Fatimah Mohammed <fmg1413@hotmail.com>
2022-09-15 10:31:26 +0300

    style pages

--- elevatorspeech/resume/models.py
+++ elevatorspeech/resume/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class resumes(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=180)
+    phone = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.name)
